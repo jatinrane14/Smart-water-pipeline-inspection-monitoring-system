7fc1a2faaa222b36a45e9ee4a755af561f70fa57
jatinrane14 <jatinrane23@gmail.com>
2026-08-31 10:47:30 +0530

    JAVA SETUP-I   Co-authored-by: Kratika Gupta <kratikaaaa2006@gmail.com>

---
+++ Backend-java/smart-pipeline-m-system/src/main/java/com/majorproject/smart_pipeline_m_system/SmartPipelineMSystemApplication.java
@@ -0,0 +1,13 @@
+package com.majorproject.smart_pipeline_m_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPipelineMSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPipelineMSystemApplication.class, args);
+	}
+
+}
---
+++ Backend-java/smart-pipeline-m-system/src/test/java/com/majorproject/smart_pipeline_m_system/SmartPipelineMSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majorproject.smart_pipeline_m_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPipelineMSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
